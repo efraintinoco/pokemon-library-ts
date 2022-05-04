ff8ca639a0050c0d62e5121855d157bfe1c3a46a
efraintinoco <oakcliff_21@yahoo.com>
2022-05-04 13:49:43 -0500

    add pokemon page

--- site/src/index.ts
+++ site/src/index.ts
@@ -6,14 +6,17 @@ const $spinner = document.querySelector<HTMLImageElement>(".spinner")
 type Pokemon = {
   name: string;
   sprites: {
-    front_default: string;
+    front_default: string[];
   };
+  url: Request | string;
+  error?: { message: string }[];
+  queryString: string[];
 };
 
 function addPokemonImage(pokemon: Pokemon) {
-    const div = document.createElement("div")
-    const titleName = `${pokemon.name[0].toUpperCase()}${pokemon.name.slice(1)}`
-    div.innerHTML = `
+  const div = document.createElement("div")
+  const titleName = `${pokemon.name[0].toUpperCase()}${pokemon.name.slice(1)}`
+  div.innerHTML = `
     <figure>
         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
         <figcaption><a href="pokemon.html?pokemon=${pokemon.name}">${titleName}</a></figcaption>
@@ -25,20 +28,20 @@ function addPokemonImage(pokemon: Pokemon) {
 }
 
 fetch(url)
-    .then(response => {
-        return response.json()
-    }).then(parsedResponse => {
-        const urls = parsedResponse.results.map(result => result.url)
-        const fetches = urls.map(url => fetch(url).then(response => response.json()))
-        return Promise.all(fetches)
-    }).then(responses => {
-      responses.forEach(response => {
-        if ($spinner) {
-          $spinner.classList.add("hidden")
-        }
-            addPokemonImage(response)
-        })
-    }).catch((error) => {
+  .then(response => {
+    return response.json()
+  }).then(parsedResponse => {
+    const urls = parsedResponse.results.map((result: Pokemon) => result.url)
+    const fetches = urls.map((url: RequestInfo) => fetch(url).then(response => response.json()))
+    return Promise.all(fetches)
+  }).then(responses => {
+    responses.forEach(response => {
+      if ($spinner) {
+        $spinner.classList.add("hidden")
+      }
+      addPokemonImage(response)
+    })
+  }).catch((error) => {
     const message = (error instanceof Error)
       ? error.message
       : "Unknown error"
