57c6c8029384b6215752e47f6b7bc372b11a4fb8
Tiffany Warren <twarre30@gmail.com>
2022-05-04 11:57:55 -0500

    first page update

---
+++ site/src/index.ts
@@ -0,0 +1,43 @@
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50"
+const $main = document.querySelector<HTMLDivElement>("main")
+const $spinner = document.querySelector<HTMLImageElement>(".spinner")
+
+
+type Pokemon = {
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+};
+
+function addPokemonImage(pokemon: Pokemon) {
+    const div = document.createElement("div")
+    const titleName = `${pokemon.name[0].toUpperCase()}${pokemon.name.slice(1)}`
+    div.innerHTML = `
+    <figure>
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+        <figcaption><a href="pokemon.html?pokemon=${pokemon.name}">${titleName}</a></figcaption>
+    </figure>
+    `
+  if ($main) {
+    $main.append(div)
+  }
+}
+
+
+
+fetch(url)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        const urls = parsedResponse.results.map(result => result.url)
+        const fetches = urls.map(url => fetch(url).then(response => response.json()))
+        return Promise.all(fetches)
+    }).then(responses => {
+      responses.forEach(response => {
+        if ($spinner) {
+          $spinner.classList.add("hidden")
+        }
+            addPokemonImage(response)
+        })
+    })
